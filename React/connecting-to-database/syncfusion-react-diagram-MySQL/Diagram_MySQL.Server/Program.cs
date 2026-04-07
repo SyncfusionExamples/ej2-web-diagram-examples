cf3cdfbe5c00dd15c5685899c49b416faa9d9396
ChellaDhuraiSSF4055 <124353845+ChellaDhuraiSSF4055@users.noreply.github.com>
2026-04-07 15:03:32 +0530

    1019669: added react mysql diagram sample

---
+++ React/connecting-to-database/syncfusion-react-diagram-MySQL/Diagram_MySQL.Server/Program.cs
@@ -0,0 +1,34 @@
+using Diagram_MySQL.Server.Data;
+using LinqToDB;
+using LinqToDB.AspNet;
+using LinqToDB.DataProvider.MySql;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddNewtonsoftJson();
+
+// CORS (dev)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("cors", p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+
+// 1) Register LinqToDB engine (connection factory) with MySQL provider
+builder.Services.AddLinqToDB(
+    (sp, options) =>
+        options.UseMySql(
+            builder.Configuration.GetConnectionString("MySqlConn"),
+            MySqlVersion.MySql80,
+            MySqlProvider.MySqlConnector
+        )
+);
+
+// 2) Register your typed DataConnection so controllers can inject it
+builder.Services.AddScoped<AppDataConnection>();
+
+var app = builder.Build();
+
+app.UseCors("cors");
+app.MapControllers();
+
+app.Run();
